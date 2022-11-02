117a931aa004f526941d862a08889636ff4bdf8c
Melissa Fields <melissamfields12@gmail.com>
2022-11-02 16:06:30 -0700

    commit tree df9541a5469f273237e8b357df1007f44a02cc2f

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //look through each integer in the array
+  for (let i = 0; i < array.length; i++) {
+    //look through the rest of the array
+   for (let j = i + 1; j < array.length; j++) {
+    //compare target to each element in the array added together
+    if (target == array[i] + array[j]) {
+      return true
+    }
+   }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Quadratic TimeL 0(n2)
 */
 
 /* 
   Add your pseudocode here
+ Add all combinations of each possible pair of numbers in the array and compare
+ the sum of each to the target number.
+ If any of the sums from the array are equal to the target number, return true. 
 */
 
 /*
   Add written explanation of your solution here
+ The function should return true if any pair of numbers in the array passed in as an
+ argument add up to the target number passed into the argument. 
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +47,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 4, 6, 8, 10], 14))
 }
 
 module.exports = hasTargetSum;
